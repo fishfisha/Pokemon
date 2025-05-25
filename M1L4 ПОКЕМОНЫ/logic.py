9e54847e85332d95f0e5eb6b8e1160bf639de085
fishfisha <mashagol10@mail.ru>
2025-05-25 14:32:02 +0300

    Update logic.py

--- M1L4 ПОКЕМОНЫ/logic.py
+++ M1L4 ПОКЕМОНЫ/logic.py
@@ -6,11 +6,14 @@ class Pokemon:
     # Инициализация объекта (конструктор)
     def __init__(self, pokemon_trainer):
 
-        self.pokemon_trainer = pokemon_trainer   
+        self.pokemon_trainer = pokemon_trainer
 
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+
+        self.power = randint(30,40)
+        self.hp = randint(200,400)
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -20,28 +23,67 @@ class Pokemon:
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            return (data['forms'][0]['name'])
+            return (data['sprites']['other']['official-artwork']['front_default'])
         else:
-            return "Pikachu"
-    
+            return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+
     # Метод для получения имени покемона через API
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            return (data['sprites']['other']['official-artwork']['front_default'])
+            return (data['forms'][0]['name'])
         else:
             return "Pikachu"
 
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"""Имя твоего покеомона: {self.name}
+        Сила покемона: {self.power}
+        Здоровье покемона: {self.hp}"""
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
 
 
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
 
+
+class Wizard(Pokemon):
+    def info(self):
+        return "у тебя покемон - волшебник! \n\n" + super().info()
+
+class Fighter (Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
+
+    def info(self):
+            return "у тебя покемон - боец! \n\n" + super().info()
+
+if __name__ == '__main__':
+    wizard = Wizard("username1")
+    fighter = Fighter("username2")
+
+    print(wizard.info())
+    print()
+    print(fighter.info())
+    print()
+    print(fighter.attack(wizard))
